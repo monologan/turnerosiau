127b3a420e223da069f4e2ed7093872878535d0b
Gerardo <monologan@gmail.com>
2019-06-24 13:07:30 -0500

    espacios index

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-
 require_once "Controladores/rutasC.php";
 require_once "Controladores/adminC.php";
 require_once "Controladores/turnosC.php";
-
 require_once "Modelos/rutasM.php";
 require_once "Modelos/adminM.php";
 require_once "Modelos/turnosM.php";
-
 $rutas = new RutasControlador();
 $rutas->Plantilla();
