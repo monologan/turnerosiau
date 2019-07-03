bf460127904a9dd015e382e0dcb17a8698def1ba
Gerardo <monologan@gmail.com>
2019-07-03 14:05:15 -0500

    ajustando cambio de numero

--- Vistas/modulos/turnoTV.php
+++ Vistas/modulos/turnoTV.php
@@ -7,22 +7,25 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-	
+
+
 </head>
 
 <body>
+	<?php header("refresh: 100"); ?>
 	<br>
 	<h1>Turnos</h1>
 	<table id="t1" border="1">
 		<thead>
 			<tr>
 				<th>Turno</th>
-				<th>Modulo</th>				
+				<th>Modulo</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -33,27 +36,30 @@
 		</tbody>
 
 	</table>
-		<audio id="audio" controls style="display:none" >
-			<source type="audio/mp3" src="audio.mp3">
-		</audio>
+	<audio id="audio" controls style="display:none">
+		<source type="audio/mp3" src="audio.mp3">
+	</audio>
 	<script type="text/javascript">
-	//audio
+		
+		//audio
 		var audio = document.getElementById("audio");
 		audio.play();
-	//blink	
-	window.setInterval(BlinkIt, 1000);
+		
+		//blink	
 		var color = "red";
 		function BlinkIt() {
 			var blink = document.getElementById("blink");
 			color = (color == "#ffffff") ? "red" : "#ffffff";
 			blink.style.color = color;
 			blink.style.fontSize = '36px';
-			window.setTimeout("BlinkIt()", 5000);
-		// 	for (var i = 900; i < 4500; i = i + 900) {
-		// 	//setTimeout("BlinkIt()", i);
-		// 	setTimeout("BlinkIt()", i + 450);
-		// }
-	}		
+			audio.play();
+		}
+		function parar() {
+			clearInterval(ads);
+		}
+		var ads = setInterval(BlinkIt, 900);
+		setTimeout(parar, 15000);
+
 	</script>
 
 
