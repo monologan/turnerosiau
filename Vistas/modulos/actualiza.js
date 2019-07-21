1271ce09d7ec87ac47dfc536dc325594e1e79a7d
Gerardo <monologan@gmail.com>
2019-07-21 11:38:34 -0500

    archivos eliminados

--- Vistas/modulos/actualiza.js
+++
@@ -1,8 +0,0 @@
-
-
-const formulario = document.getElementById('tercero'); 
-console.log(formulario);
-formulario.addEventListener("click", function(){ 
-	var ventana = 'http://localhost/mvc/index.php?ruta=turnoTV';
-	window.location.reload();
- }); 
--- Vistas/modulos/seccion.php
+++
@@ -1,7 +0,0 @@
-<div class="text-white my-5 py-5">
-
-<?php 
-
-	echo rand(1,800);
- ?>
- </div>
