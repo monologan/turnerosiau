6cee3abb2894a9fe27147683ed93997e9f2e4d1e
Gerardo <monologan@gmail.com>
2019-07-20 21:45:19 -0500

    turnoc arreglado

--- Controladores/turnosC.php
+++ Controladores/turnosC.php
@@ -13,16 +13,11 @@ class TurnosC
 
             $resultado = TurnosM::RegistrarTurnoM($datosC, $tablaBD);
 
-<<<<<<< HEAD
-            if ($resultado == "bien") {
-                header("location:index.php?ruta=modulo");    
-=======
-            if ($resultado == "bien") {                
-                header("location:index.php?ruta=modulo");           
-                
->>>>>>> b5be3e5b13928f07388c6a9520313438cb3fefee
+            if($resultado == "bien"){
+
+                header("location:index.php?ruta=modulo");
             } else {
-                echo "Turno en Atencion.";
+                echo "Turno en Atencion.  Por favor ingrese el siguiente";
             }
         }
     }
@@ -33,21 +28,13 @@ class TurnosC
         $tablaBD2 = "administradores";
 
         $respuesta = TurnosM::MostrarTurnosM($tablaBD, $tablaBD2);
-        
+
         foreach ($respuesta as $key => $value) {
-            
             echo '<tr id="blink">
-            <td ><h1 class="display-5">' . $value["turno"] . '</h1></td>    
-            <td ><h1 class="display-5">' . $value["usuario"] . '</h1></td>                
+            <td >' . $value["turno"] . '</td>    
+            <td >' . $value["usuario"] . '</td>               
+            
         </tr>';
-        }
-<<<<<<< HEAD
-      
-=======
-        
->>>>>>> b5be3e5b13928f07388c6a9520313438cb3fefee
+        }        
     }
 }
-?>
-
-
